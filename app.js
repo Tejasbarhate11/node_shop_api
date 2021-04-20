ea5003247248a5c548024b47482f5a74ffe9f3e7
Tejasbarhate11 <tjbarhate11@gmail.com>
2021-04-20 23:09:44 +0530

    error handling

--- app.js
+++ app.js
@@ -5,8 +5,24 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//Routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found baby!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 
 module.exports = app;
